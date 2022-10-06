bbaddeb8b5ec46a88f659b92004f97a77a5a46dd
blxck-kokoro <blxckkokoro@gmail.com>
2022-10-06 14:38:34 +0200

    Created dishes' objects in dishes' objects section

--- restaurant.c
+++ restaurant.c
@@ -19,7 +19,39 @@ int main(void)
         scanf("%c", &menuSwitch);
     }
 
-    int appetizersSize = getFileLines("Dishes/appetizers.txt") / 2;
+    //  ***     DISHES' OBJECTS SECTION      ***
+    //  Appetizers objects
+    char *appetizersFilePath = "Dishes/appetizers.txt";
+    int appetizersSize = getFileLines(appetizersFilePath) / 2;
     dish *appetizers = malloc(appetizersSize * sizeof(dish));
-    makeDishesArray(appetizers, appetizersSize, "Dishes/appetizers.txt");
+    makeDishesArray(appetizers, appetizersSize, appetizersFilePath);
+    freeDishes(appetizers, appetizersSize);
+
+    // Soups objects
+    char *soupsFilePath = "Dishes/soups.txt";
+    int soupsSize = getFileLines(soupsFilePath) / 2;
+    dish *soups = malloc(soupsSize * sizeof(dish));
+    makeDishesArray(soups, soupsSize, soupsFilePath);
+    freeDishes(soups, soupsSize);
+
+    // Main Courses objects
+    char *mainCoursesFilePath = "Dishes/mainCourses.txt";
+    int mainCoursesSize = getFileLines(mainCoursesFilePath) / 2;
+    dish *mainCourses = malloc(mainCoursesSize * sizeof(dish));
+    makeDishesArray(mainCourses, mainCoursesSize, mainCoursesFilePath);
+    freeDishes(mainCourses, mainCoursesSize);
+
+    //  Desserts
+    char *dessertsFilePath = "Dishes/desserts.txt";
+    int dessertsSize = getFileLines(dessertsFilePath) / 2;
+    dish *desserts = malloc(dessertsSize * sizeof(dish));
+    makeDishesArray(desserts, dessertsSize, dessertsFilePath);
+    freeDishes(desserts, dessertsSize);
+
+    //  Beverages
+    char *beveragesFilePath = "Dishes/beverages.txt";
+    int beveragesSize = getFileLines(beveragesFilePath) / 2;
+    dish *beverages = malloc(beveragesSize * sizeof(dish));
+    makeDishesArray(beverages, beveragesSize, beveragesFilePath);
+    freeDishes(beverages, beveragesSize);
 }
